53e37761404ab32e6037027db8b5583f9d344e9e
Ahmed Elbuome <ahmedelbauome1111@gmail.com>
2023-11-26 23:26:32 +0200

    add class [post admin]

--- post/models.py
+++ post/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
     content = models.TextField(max_length=1000)
     created_at = models.DateTimeField(default= timezone.now)
     image = models.ImageField(upload_to=image_upload)
-
+    active = models.BooleanField(default=False)
 
     class Meta:
         verbose_name = ("Post")
--- post/views.py
+++ post/views.py
@@ -5,7 +5,27 @@ from .models import Post
 # Create your views here.
 
 class PostList(ListView):
+    
+    # context name in normal equal ['object_list', 'modelname_list']
+    
     model = Post
+
+    context_object_name = 'all_posts'
+    ordering = ['-created_at']
+    # queryset = Post.objects.filter(active=True)
+    # template_name = 'post/test.html'
+    
+    def get_queryset(self):
+        return Post.objects.filter(active=True)
+    
+    def get_context_data(self, **kwargs) :
+        context = super().get_context_data(**kwargs)
+        context["my_name"] = 'Ahmed'
+        return context
+    
+
+
+
 
 class PostDetail(DetailView):
     model = Post
